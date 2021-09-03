41a27225ceb5b02c89b70f033c97dc6be9d89868
Simran Kaur <77620422+SimranKaur431@users.noreply.github.com>
2021-09-03 17:33:12 +0530

    Add files via upload

--- Assignment1.py
+++ Assignment1.py
@@ -3,8 +3,10 @@ numbers = [386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615,
  687, 217, 815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445,
  742, 717, 958,743, 527]
 for num in numbers:
-    if(num<237 and num%2==0):
-       print(num)
+    if(num==237):
+        break
+    elif (num%2==0):
+        print(num)
 ########################################
 a = int(input("Input an integer : "))
 n1 = int( "%s" % a )
@@ -30,7 +32,7 @@ else:
     
 
 a_string = str(input())
-count = 0;
+count = 0
 string1 = ""
 string2 = ""
 numbers1 = []
